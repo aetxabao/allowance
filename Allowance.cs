8c50be29b1800d752b661f45f52dbe9e004ecbc6
aetxabao <aitor.etxabarren@mariaanasanz.es>
2020-10-22 18:21:48 +0200

    1 account for each one

--- Allowance.cs
+++ Allowance.cs
@@ -4,40 +4,39 @@ namespace allowance
 {
     class Allowance
     {
-        private Account account;
+        private Account[] accounts;
         private Parent[] parents;
         private Child[] children;
 
-        public void Run(int r, int n, int a, int m, int p, int c1, int c2, int q, int t)
+        public void Run(int x, int a, int m, int p, int c1, int c2, int q, int t)
         {
             Console.WriteLine("Saldo inicial: {0}, saldo mínimo posible: {1}", a, m);
-            Console.WriteLine("Ingreso semanal: {0}, parents:{1}", p, r);
-            Console.WriteLine("Gastos semanales:[{0},{1}], children:{2}", c1, c2, n);
-            Console.WriteLine("Probabilidad de que gasten 10 veces más:{0}", q);
+            Console.WriteLine("Ingreso semanal: {0}, parents:{1}", p, x);
+            Console.WriteLine("Gastos semanales: [{0},{1}], children: {2}", c1, c2, x);
+            Console.WriteLine("Probabilidad de que gasten 10 veces más: {0}%", q);
             Console.WriteLine("Timeout gasto:{0}", t);
-            Init(r, n, a, m, p, c1, c2, q, t);
+            Init(x, a, m, p, c1, c2, q, t);
             Start();
             Finish();
             Console.WriteLine("Depositos cada parent: {0}", parents[0].Deposit);
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < x; i++)
             {
                 Console.WriteLine("Child {0} ha gastado {1} y ha sido bloqueado {2}", i, children[i].Expenses, children[i].Blocks);
+                Console.WriteLine("Saldo final de su cuenta: {0}", accounts[i].Balance);
             }
-            Console.WriteLine("Saldo final: {0}", account.Balance);
         }
 
-        private void Init(int r, int n, int a, int m, int p, int c1, int c2, int q, int t)
+        private void Init(int x, int a, int m, int p, int c1, int c2, int q, int t)
         {
-            account = new Account(a, m, t);
-            parents = new Parent[r];
-            for (int i = 0; i < r; i++)
+
+            accounts = new Account[x];
+            parents = new Parent[x];
+            children = new Child[x];
+            for (int i = 0; i < x; i++)
             {
-                parents[i] = new Parent(account, p);
-            }
-            children = new Child[n];
-            for (int i = 0; i < n; i++)
-            {
-                children[i] = new Child(account, c1, c2, q);
+                accounts[i] = new Account(a, m, t);
+                parents[i] = new Parent(accounts[i], p);
+                children[i] = new Child(accounts[i], c1, c2, q);
             }
         }
 
@@ -58,20 +57,19 @@ namespace allowance
             {
                 parents[i].Finish();
             }
-            account.Close();
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                accounts[i].Close();
+            }
             for (int i = 0; i < children.Length; i++)
             {
                 children[i].Finish();
             }
         }
-        public int Balance()
-        {
-            return account.Balance;
-        }
         static void Main(string[] args)
         {
             Allowance p = new Allowance();
-            p.Run(2, 3, 0, -100, 10, 3, 8, 10, 1);
+            p.Run(3, 0, -10, 5, 3, 8, 10, 1);
         }
 
     }
